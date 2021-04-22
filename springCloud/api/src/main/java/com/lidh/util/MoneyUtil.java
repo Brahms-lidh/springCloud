396cb52b53cb210992a274912d65b274b44b94dd
lidh <867438324@qq.com>
2021-04-22 09:48:58 +0800

    提交测试金钱操作工具类

---
+++ springCloud/api/src/main/java/com/lidh/util/MoneyUtil.java
@@ -0,0 +1,13 @@
+package com.lidh.util;
+
+/**
+ * @description: 金钱操作工具类
+ * @projectName:springCloud
+ * @see:com.lidh.util
+ * @author:荔殿虎
+ * @createTime:2021/4/22 9:47
+ * @version:1.0
+ */
+public class MoneyUtil {
+
+}
